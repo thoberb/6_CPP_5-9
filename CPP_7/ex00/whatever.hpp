e2e12e77426cd6969617a6143f6cbec9409b124a
blandine berthod <blandineberthod@mac-studio-de-blandine.home>
2024-03-26 19:14:39 +0100

    ex00 fini

---
+++ CPP_7/ex00/whatever.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blandineberthod <blandineberthod@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 19:09:43 by blandineber       #+#    #+#             */
+/*   Updated: 2024/03/26 19:13:37 by blandineber      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	return (a > b ? a : b);
+}
+
+#endif
